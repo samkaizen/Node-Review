f36df85c5211b59ba3c84ef959733c421f76cc64
Sam <amhajsaid@gmail.com>
2018-02-15 22:44:21 +0000

    adding body-parser

--- templating.js
+++ templating.js
@@ -21,7 +21,7 @@ app.get('/profile/:name', (req,res)=>{
     //This is how we send html files with render(): so 'profile' is the name of the file that is onside the views folder (ejs automatically goes to that folder ),t in the second parametre is the the data that we want to dynamically inject in the profile.ejs file to serve to the user, in this exaple we grabbed the name that any given user input
     //as req.params.name
     // the next step is to flip back over the profileejs and just dynamically inject this data (person) => go to profile.ejs to see that !
-    const ninja = { age : 33 , belt : 'Black'}
+    const ninja = { age : 33 , belt : 'Black' , hobbies : ['Fighting', 'Coding','Dating','hacking']}
      const {name} = req.params
     res.render('profile', { name :name , ninja : ninja});
 });
